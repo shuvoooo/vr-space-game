7af1347882231842805bb94a9e95fe75e1d389e9
shuvoooo <shuvo.punam@gmail.com>
2022-12-11 18:24:19 +0530

    Initial

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Asteroid : MonoBehaviour
+{
+    
+ 
+    public GameObject player;
+    Player ps;
+    int numberOfAsteriod;
+    [SerializeField]
+    float tumble;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+        player = GameObject.Find("Player");
+        ps = player.GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        var user = GameObject.FindGameObjectsWithTag("player");
+        Vector3 userPosition = player.transform.position;
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, userPosition, 5 * Time.deltaTime);
+    }
+
+
+    public void asteroidDestroy()
+    {
+     
+        GetComponent<AudioSource>().Play();
+        numberOfAsteriod = GameObject.FindGameObjectsWithTag("asteroid").Length;
+        if(numberOfAsteriod<=20)
+        {
+            GameObject asteroid = Instantiate(Resources.Load("Asteroid1")) as GameObject;
+            float x = Random.Range(-35f, 35f);
+            float z = Random.Range(45f, 70f);
+            float y = Random.Range(10f, 20f);
+            asteroid.transform.position = new Vector3(x, y, z);
+        }
+        
+        ps.shoot();
+        Destroy(gameObject, .3f);
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            Destroy(gameObject);
+
+            GameObject asteroid = Instantiate(Resources.Load("Asteroid1")) as GameObject;
+            float x = Random.Range(-35f, 35f);
+            float z = Random.Range(45f, 70f);
+            float y = Random.Range(5f, 25f);
+            asteroid.transform.position = new Vector3(x, y, z);
+        }
+           
+
+    }
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    CharacterController characterController;
+    public Transform cameraTransform;
+    GameObject gun;
+    public TextMeshProUGUI textMeshProUGUI;
+    public TextMeshProUGUI textMeshProUGUI2;
+    int finalScore;
+    bool gameOver = false;
+    public int score = 0;
+    public int life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        gun = gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
+        score = 0;
+        life = 3;
+        textMeshProUGUI.text = "Score: " + score + "\nLife: " + life;
+    }
+
+    public void shoot()
+    {
+        StartCoroutine("Shoot");
+    }
+
+    IEnumerator Shoot()
+    {
+        // gun.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(1f);
+        score += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMeshProUGUI.text = "Score: " + score + "\nLife: " + life;
+        if (gameOver == true)
+        {
+            finalScore = score;
+
+            textMeshProUGUI2.text = "Game Over" + "\n Your Score: " + finalScore;
+        }
+
+
+        // camera move forward with gravity
+        Vector3 move = new Vector3(0, 0, 1);
+        move = cameraTransform.TransformDirection(move);
+        move *= 0.1f;
+        characterController.Move(move);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "asteroid")
+        {
+            if (life <= 0)
+            {
+                life = 0;
+                gameOver = true;
+            }
+
+            if (life > 0)
+            {
+                life--;
+            }
+
+            Debug.Log("hello");
+        }
+    }
+}
